46c06a76f135d6e34593d242cb41a8eb3b3d5039
NovichenkoYura <novichenko.y@gmail.com>
2023-04-05 16:43:57 +0300

    main foto

---
+++ src/components/FrontView/FrontView.jsx
@@ -0,0 +1,8 @@
+import './FrontView.css';
+export const FrontView = () => {
+  return (
+    <section className="container frontview">
+      <div></div>
+    </section>
+  );
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,20 @@
-import './Header.module.css';
+import './Header.css';
 export const Header = () => {
-  return <div className="container">test</div>;
+  return (
+    <header>
+      <div className="container header__wrap">
+        <svg width="104" height="26" className="header__icon">
+          <use href="../../images/Logo_optimized.svg"></use>
+        </svg>
+        <div className="header__buttonlist">
+          <button type="button" className="header__button">
+            Users
+          </button>
+          <button type="button" className="header__button">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
