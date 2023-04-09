33700412133a31cb64b2116aa7c2e17b12072de8
NovichenkoYura <novichenko.y@gmail.com>
2023-04-09 18:30:43 +0300

    prices

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -16,12 +16,55 @@ export const Pricing = () => {
             <div className="pricing__details">
               <h4 className="pricing__title">basic</h4>
               <span className="pricing__price">40</span>
-              <ul className="pricing__description">
+              <ul className="pricing__description list">
                 <li>Five Website</li>
                 <li>Five User</li>
                 <li>100 GB Bandwidth</li>
                 <li>2 GB Storage</li>
               </ul>
+            </div>
+            <div className=" ">
+              <a href="" className="pricing__btn link">
+                Purchase Now
+              </a>
+            </div>
+          </div>
+        </li>
+        <li className="pricing__item">
+          <div className="pricing__content">
+            <div className="pricing__details">
+              <h4 className="pricing__title">basic</h4>
+              <span className="pricing__price">50</span>
+              <ul className="pricing__description list">
+                <li>20 Website</li>
+                <li>20 User</li>
+                <li>200 GB Bandwidth</li>
+                <li>20 GB Storage</li>
+              </ul>
+            </div>
+            <div className=" ">
+              <a href="" className="pricing__btn link">
+                Purchase Now
+              </a>
+            </div>
+          </div>
+        </li>
+        <li className="pricing__item">
+          <div className="pricing__content">
+            <div className="pricing__details">
+              <h4 className="pricing__title">unlimited</h4>
+              <span className="pricing__price">60</span>
+              <ul className="pricing__description list">
+                <li>Unlimited Website</li>
+                <li>Unlimited User</li>
+                <li>Unlimited Bandwidth</li>
+                <li>Unlimited Storage</li>
+              </ul>
+            </div>
+            <div className=" ">
+              <a href="" className="pricing__btn link">
+                Purchase Now
+              </a>
             </div>
           </div>
         </li>
